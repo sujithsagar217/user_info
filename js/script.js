8c5a0c3c00d35519546f6ec2bfc3be79de84352a
Sujith <gdvsujithsagar217@gmail.com>
2020-01-24 12:29:50 +0530

    added form and linked with firebase

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  M.updateTextFields();
+});
+
+
+
+
+
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyASLy5INnJk1093cO6rNdA_s4YBxlH59kA",
+  authDomain: "form-d1202.firebaseapp.com",
+  databaseURL: "https://form-d1202.firebaseio.com",
+  projectId: "form-d1202",
+  storageBucket: "form-d1202.appspot.com",
+  messagingSenderId: "785871844861",
+  appId: "1:785871844861:web:5328063510bd1e6e4e9594"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
